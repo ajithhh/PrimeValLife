6bbe2dfc7833be5ffcbe352eca885a376770e1f2
ajithk <ajitt.kumar.k@gmail.com>
2023-12-30 03:50:20 +0530

    login

--- TUT.IAuth/Models/TUTUser.cs
+++ TUT.IAuth/Models/TUTUser.cs
@@ -4,4 +4,9 @@ public class TUTUser
 {
     public required string UserName { get; set; }
     public required string Password { get; set; }
+    public bool RememberMe { get; set; }
+    public TUTUser()
+    {
+        RememberMe = false;
+    }
 }
