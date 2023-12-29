65036c951a8f1ee791de2ffde2ab5d3a288cd25d
Arun01-hub <arun17012001@gmail.com>
2023-12-29 21:16:29 +0530

    Activation repository service

--- PrimeValLife.Core/Infrastructure/ResolveDependencyBuilder.cs
+++ PrimeValLife.Core/Infrastructure/ResolveDependencyBuilder.cs
@@ -1,7 +1,9 @@
 ﻿namespace PrimeValLife.Core.Infrastructure;
 
 using Microsoft.Extensions.DependencyInjection;
+using PrimeValLife.Core.IRepository;
 using PrimeValLife.Core.IServices;
+using PrimeValLife.Core.Repository;
 using PrimeValLife.Core.Services;
 using TUT.IAuth.IServices;
 using TUT.IAuth.Services;
@@ -11,6 +13,7 @@ public static class ResolveDependencyBuilder
     public static void ResolveDependency(this IServiceCollection services)
     {
         services.AddTransient<IIdentityService, IdentityService>();
+        services.AddTransient<IAuditRepository,AuditRepository>();
         services.AddTransient<IAuditService, AuditService>();
     }
 }
